5d5b6e7eadc7cf24b76e4f6973f3f8fb524ba7aa
Hongling Zhang <honglingzhang@Honglings-MacBook-Pro.local>
2022-08-08 00:08:23 -0600

    add a new EJS tool and use bootstrap5

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const cities  = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
+const axios = require('axios');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 .then(() => {
@@ -12,15 +13,43 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp')
   console.log(err);
 })
 
+ // call unsplash and return small image
+ async function seedImg() {
+  try {
+    const resp = await axios.get('https://api.unsplash.com/photos/random', {
+      params: {
+        client_id: 'SaigZfpcGCMiGSlXoGfTwmntcEa8a6vxhP3Q2u7Gji4',
+        collections: 1114848,
+      },
+    })
+    return resp.data.urls.small;
+  } catch (err) {
+    console.error(err)
+  }
+}
+
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
   for(let i = 0; i < 15; i++) {
+    // setup
     const random15 = Math.floor(Math.random() * 15);
+    const price = Math.floor(Math.random() * 20) + 10;
+
+    // const placeSeed = Math.floor(Math.random() * places.length)
+    // const descriptorsSeed = Math.floor(Math.random() * descriptors.length)
+    // const citySeed = Math.floor(Math.random() * cities.length)
+
+    // seed data into campground
     const camp = new Campground({
       location: `${cities[random15].city}, ${cities[random15].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`
+      title: `${sample(descriptors)} ${sample(places)}`,
+      // title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
+      // location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+      image: await seedImg(),
+      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
+      price
     })
     await camp.save();
   }
