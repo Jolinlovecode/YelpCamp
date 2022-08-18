6591c994fa02354f658e877b682d1b26d4544ba5
Hongling Zhang <jolinhlzhang@gmail.com>
2022-08-17 23:58:39 -0600

    Add Deleting reviews and Campground Delete Middleware

--- app.js
+++ app.js
@@ -70,9 +70,10 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
   res.redirect(`/campgrounds/${campground._id}`)
 }))
 
+
 app.get("/campgrounds/:id", catchAsync(async (req, res) => {
   const campground = await Campground.findById(req.params.id).populate('reviews');
-  console.log(campground);
+  // console.log(campground);
   res.render("campgrounds/show", { campground });
 }))
 
@@ -103,6 +104,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
   res.redirect(`/campgrounds/${campground._id}`);
 }))
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res,next) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: {reviews: reviewId }});
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
+
+}))
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
