30e0a4c4a9ea1116ff8437fb8cfb21fb5baa1540
Hongling Zhang <jolinhlzhang@gmail.com>
2022-09-09 11:44:15 -0600

    Add a Review and User Controller

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,19 +3,10 @@ const router = express.Router({ mergeParams:true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const Campground = require("../models/campground");
 const Review = require("../models/review");
+const reviews = require('../controllers/reviews');
 const catchAsync = require('../utils/catchAsync');
 
-router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
-  // console.log(req.params);
-  const campground = await Campground.findById(req.params.id);
-  const review = new Review(req.body.review);
-  review.author = req.user._id;
-  campground.reviews.push(review);
-  await review.save();
-  await campground.save();
-  req.flash('success', 'Created new review!')
-  res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
 router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res,next) => {
   const { id, reviewId } = req.params;
