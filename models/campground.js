2b92bc28f9961570bf1b439f73f17edd06ad9502
Hongling Zhang <jolinhlzhang@gmail.com>
2022-09-19 15:34:04 -0600

    Add custom Popups

--- models/campground.js
+++ models/campground.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Review = require("./review");
 const Schema = mongoose.Schema;
 
+const opts = { toJSON: { virtuals: true } };
+
 const CampgroundSchema = new Schema({
   title: String,
   images: [
@@ -33,8 +35,8 @@ const CampgroundSchema = new Schema({
       type: Schema.Types.ObjectId,
       ref: "Review",
     },
-  ],
-});
+  ]
+}, opts);
 
 // here findOneAndDelete keep the same middleware app.js line 94 findByIdAndDelete
 CampgroundSchema.post("findOneAndDelete", async function (doc) {
@@ -49,6 +51,11 @@ CampgroundSchema.post("findOneAndDelete", async function (doc) {
 
 CampgroundSchema.path('images').schema.virtual('thumbnail').get(function() {
   return this.url.replace('/upload/', '/upload/w_200/');
+});
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function(){
+  return `<strong><a href="/campgrounds/${this._id}">${this.title}</a></strong>
+  <p>${this.description.substring(0,20)}...</p>`
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
